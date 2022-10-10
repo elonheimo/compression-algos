6b1de39c90f2a04c012df086d432c94fc0d89c03
Johannes Elonheimo <johannes.elonheimo@gmail.com>
2022-10-10 21:05:25 +0300

    add working huffman coding implemention and docstring

--- src/huffman/huffman.py
+++ src/huffman/huffman.py
@@ -1,11 +1,16 @@
 from collections import defaultdict
 import heapq
-from typing import final
 from bitarray import bitarray
 
 
 class HuffmanCoding:
     def __init__(self, input_path :str, output_path :str) -> None:
+        """Class that encodes and decodes files with Huffman coding
+
+        Args:
+            input_path (str): 
+            output_path (str):
+        """
         self.input_file_path = input_path
         self.output_file_path = output_path
 
@@ -17,21 +22,57 @@ class HuffmanCoding:
 
         def __lt__(self, other):
             if self.frequency < other.frequency:
-                return False
-            return True
+                return True
+            return False
 
+        def __str__(self) -> str:
+            ret = "\n"
+            def loop_layers(node, level) ->str:
+                if node.left:
+                    loop_layers(node.left, level+1)
+                if node is not None:
+                    nonlocal ret
+                    ret += (' ' * 4 * level + '-> ' + str(node.content)+ "\n")
+                if node.right:
+                    loop_layers(node.right, level+1)
+            loop_layers(self, 0)
+            return ret
+                
+                
 
-    def frequency_dict(self, byte_list :list):
+    def frequency_dict(self, byte_list :list) -> dict:
+        """Creates dictionary with keys as bytes and values as their count
+
+        Args:
+            byte_list (list): list of bytes represented as ints
+
+        Returns:
+            dict: _description_
+        """
         freq_dict = defaultdict(lambda: 0)
         for i in byte_list:
+            # note that
+            # bytes type of list elements are ints not bytes
+            # dict[int] = int
             freq_dict[i] += 1
         return freq_dict
 
     def create_tree(self, freq_dict :dict) -> TreeNode:
+        """Creates binarytree from a dictionary of freaquencies
+
+        Args:
+            freq_dict (dict): dict[int] = int
+
+        Returns:
+            TreeNode: Root node of binarytree
+        """
         heap_list = []
         for key, value in freq_dict.items():
-            #key = byte, value = int
-            node = self.TreeNode(key, value)
+            #key = byte_as_int, value = frequency of byte as int
+            node = self.TreeNode(
+                key,
+                value
+            )
             heapq.heappush(heap_list, node)
 
         while len(heap_list) >1:
@@ -44,9 +85,17 @@ class HuffmanCoding:
 
             heapq.heappush(heap_list, parent)
    
-        return heap_list[1] # returns root node
+        return heap_list[0] # returns root node
 
-    def huffman_codes(self, root: TreeNode):
+    def huffman_codes(self, root: TreeNode) -> dict:
+        """Creates dictionary of every databyte to unique Huffman code
+
+        Args:
+            root (TreeNode): root node
+
+        Returns:
+            dict: dict[int] = bitarray
+        """
         byte_to_code_dict = {}
 
         def dict_filler(parent, code :bitarray):
@@ -57,11 +106,11 @@ class HuffmanCoding:
                 return
             dict_filler(
                 parent.left,
-                code + bitarray('0')
+                code + bitarray('1')
             )
             dict_filler(
                 parent.right,
-                code + bitarray('1')
+                code + bitarray('0')
             )
         dict_filler(
             root,
@@ -69,23 +118,25 @@ class HuffmanCoding:
         )
 
         return byte_to_code_dict
-
-    def read_input(self) -> bytes:
-        try:
-            # rb = read binary
-            with open(self.input_file_path, 'rb') as input_file:
-                return input_file.read()
-        except IOError:
-            return print(f"could not open {self.input_file_path}")
     
     def tree_to_bitarray(self, root :TreeNode) -> bitarray:
+        """Generates bitarray representing huffman tree
+
+        Args:
+            root (TreeNode): root node
+
+        Returns:
+            bitarray
+        """
         route_buffer = bitarray('')
         def helper(node):
             if node is None:
                 return
             if node.content:
                 route_buffer.append(1)
-                route_buffer.frombytes(node.content)
+                route_buffer.frombytes(
+                    node.content.to_bytes(1, byteorder='big')
+                )
             else:
                 route_buffer.append(0)
             helper(
@@ -97,8 +148,16 @@ class HuffmanCoding:
         helper(root)
         return route_buffer
 
-    def header_to_binarytree(self, data :bitarray) -> TreeNode:
+    def header_to_binarytree(self, data :bitarray, start_index: int) -> tuple:
+        """Creates binary tree of huffman codes from binary representing it
 
+        Args:
+            data (bitarray): data
+            start_index (int): part of header where binary tree starts
+
+        Returns:
+            tuple: (root, index) root node of binary tree and index is the last bit of header
+        """
         def helper(node, i):
             if data[i] == 0:
                 node.left = self.TreeNode(None, 0)
@@ -112,35 +171,115 @@ class HuffmanCoding:
             elif data[i] == 1:
                 #read one byte
                 i += 1
-                node.content = data[i: i+8]#.tobytes()
-                print(data[i: i+8].tobytes())
+                node.content = data[i: i+8].tobytes()[0]
                 i += 7
             return i
 
         root= self.TreeNode(None, 0)
-        helper(root, i = 0)
-        return root
+        index = helper(root, start_index)
+        return (root, index)
 
 
     def encode(self):
+        """Encodes input file with huffman coding and saves to outputfile
+        """
         input_data = self.read_input()
         freq_dict = self.frequency_dict(input_data)
         root_node = self.create_tree(freq_dict)
         huffman_codes = self.huffman_codes(root_node)
         temp_buffer = self.tree_to_bitarray(root_node)
-        for byte in input_data:
-            temp_buffer += huffman_codes[byte]
-        huffman_table_bitarray = self.tree_to_bitarray(root_node)
-        discard_bits = (
-            (len(huffman_table_bitarray)
-            + len(temp_buffer)
-            -1 )
-            % 8
+        # buffer
+        # |huffmancodes|
+        for byte_as_int in input_data:
+            temp_buffer.extend(
+                huffman_codes[byte_as_int]
+            )
+        # buffer
+        # |huffmancodes|encoded bytes|
+        tail_bits = (
+            8 -
+            ((len(temp_buffer) +3 ) % 8)
         )
+        if tail_bits == 8:
+            tail_bits = 0
         final_bitarray = bitarray('')
         final_bitarray.frombytes(
-            bytes([discard_bits])
+            bytes([tail_bits])
         )
         final_bitarray = final_bitarray[5:]
         final_bitarray += temp_buffer
+        final_bitarray.fill() #add tail bits
+        # buffer
+        # 0 bits amount|huffmancodes|encoded bytes|0bits
+        self.save_output(
+            final_bitarray.tobytes()
+        )
+    
+    def encoded_tail_length(self, data: bitarray) -> int:
+        """Returns the tail length of encoded data as int
+
+        Args:
+            data (bitarray): data
+
+        Returns:
+            int: length of tail bits to ignore
+        """
+        discard_tail_bits = bitarray('00000')
+        discard_tail_bits += data[:3]
+        return int.from_bytes(
+            discard_tail_bits.tobytes(),
+            'big'
+        )
+
+    def decode(self):
+        """Decodes input_file and saves as outputfile
+        """
+        data = bitarray('')
+        data.frombytes(
+            self.read_input()
+        )
+        tail_length = self.encoded_tail_length(data)
+        (root, index) = self.header_to_binarytree(data, 3 )
+        index += 1
+        buffer = []#[bytearray()]
+        current_node = root
+        while index < len(data) - tail_length:
+            if data[index] == 1:
+                current_node = current_node.left
+            else:
+                current_node = current_node.right
+
+            if current_node.content:
+                buffer.append(current_node.content)
+                current_node = root
+            index += 1
+        self.save_output(bytes(buffer))
+
+    def save_output(self, buffer: bytes):
+        """Saves data
+
+        Args:
+            buffer (bytes): data to save
+        """
+        try:
+            # wb = write binary
+            with open(self.output_file_path, 'wb') as output_file:
+                output_file.write(buffer)
+            print(f"Succesfully saved {self.output_file_path}")
+        except IOError:
+            print("Error while saving output {file_path}")
+
+
+    def read_input(self) -> bytes:
+        """Read objects input file
+
+        Returns:
+            bytes: data as bytes
+        """
+        try:
+            # rb = read binary
+            with open(self.input_file_path, 'rb') as input_file:
+                return input_file.read()
+        except IOError:
+            return print(f"could not open {self.input_file_path}")
         
--- src/tests/huffman_test.py
+++ src/tests/huffman_test.py
@@ -23,39 +23,75 @@ class TestHuffmanCoding(unittest.TestCase):
         testbuffer.frombytes(str.encode("E"))
         return testbuffer
 
-    def test_tree_to_bitarray(self):
+    def make_test_tree(self) -> HuffmanCoding.TreeNode:
         huffman_coding = HuffmanCoding("","")
-
         root = huffman_coding.TreeNode(None, 0)
         root.left = huffman_coding.TreeNode(None, 0)
         root.left.left = huffman_coding.TreeNode(None, 0)
         root.left.left.left = huffman_coding.TreeNode(
-            str.encode("A"), 0
+            int.from_bytes(str.encode("A"), byteorder='big'), 0
         )
         root.left.left.right = huffman_coding.TreeNode(
-            str.encode("B"), 0
+            int.from_bytes(str.encode("B"), byteorder='big'), 0
         )
         root.left.right = huffman_coding.TreeNode(
-            str.encode("C"), 0
+            int.from_bytes(str.encode("C"), byteorder='big'), 0
         )
         root.right = huffman_coding.TreeNode(None, 0)
         root.right.left = huffman_coding.TreeNode(
-            str.encode("D"), 0
+            int.from_bytes(str.encode("D"), byteorder='big'), 0
         )
         root.right.right = huffman_coding.TreeNode(
-            str.encode("E"), 0
+            int.from_bytes(str.encode("E"), byteorder='big'), 0
+        )
+        return root
+
+    def test_encode(self):
+        input_path = "samples/munkki_kammio.txt"
+        encoded = "testing_files/munkki_kammio.txt.hc"
+        decoded = "testing_files/munkki_kammio_hc.txt"
+        HuffmanCoding(input_path, encoded).encode()
+        HuffmanCoding(encoded, decoded).decode()
+        with open(input_path, "rb") as original, open(decoded, "rb") as new:
+            original_str = original.read()
+            new_str = new.read()
+            self.assertEqual(original_str, new_str)
+
+    def test_huffman_codes(self):
+        huffman_coding = HuffmanCoding("","")
+        byte_to_code_dict = huffman_coding.huffman_codes(
+            self.make_test_tree()
         )
         self.assertEqual(
+            byte_to_code_dict[int.from_bytes(str.encode("A"), byteorder='big')],
+            bitarray('111')
+        )
+
+    def test_tree_to_bitarray(self):
+        huffman_coding = HuffmanCoding("","")
+
+        self.assertEqual(
             self.make_test_bitarray(),
-            huffman_coding.tree_to_bitarray(root)
+            huffman_coding.tree_to_bitarray(
+                self.make_test_tree()
+            )
         )
 
     def test_header_to_binarytree(self):
         huffman_coding = HuffmanCoding("","")
-        test_buffer = self.make_test_bitarray()
-        root_from_test_buffer = huffman_coding.header_to_binarytree(test_buffer)
+        og_test_buffer = self.make_test_bitarray()
+        test_buffer_with_tail_bits = og_test_buffer + bitarray('01110')
+        (root_from_test_buffer, index) = huffman_coding.header_to_binarytree(test_buffer_with_tail_bits, 0)
         new_buffer = huffman_coding.tree_to_bitarray(root_from_test_buffer)
         self.assertEqual(
-            test_buffer,
+            og_test_buffer,
             new_buffer
+        )
+
+    def test_encoded_tail_length(self):
+        data = bitarray('11100000')
+        huffman_coding = HuffmanCoding("","")
+        self.assertEqual(
+            huffman_coding.encoded_tail_length(data),
+            7
         )
