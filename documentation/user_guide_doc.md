0d17a0b2ce370b3db42371cfae5660955475467a
Johannes Elonheimo <johannes.elonheimo@gmail.com>
2022-10-17 17:08:39 +0300

    Initial uder guide

---
+++ documentation/user_guide_doc.md
@@ -0,0 +1,56 @@
+# User guide
+
+## Installation
+
+Install [git](https://www.atlassian.com/git/tutorials/install-git) on your system.
+
+Check your local version with ```python3 --version``` 
+
+Install [python](https://realpython.com/installing-python/) version 3.8.10 if not installed.
+
+Clone the github repository
+
+```git clone https://github.com/elonheimo/compression-algos```
+
+Change directory to downloaded folder
+
+Check that you have poetry version 1.2.2 with ```poetry --version```
+
+Install [poetry](https://pypi.org/project/poetry/) if not installed.
+
+Run ```poetry install```
+
+Further instructions on use are under command line interface.
+
+## Cli, Command line interface
+
+### Example encode&decode file
+
+Encode
+
+```python3 main.py -i <input file path> -o <output_file_path> <algorithm -lz or -hz>```
+
+```python3 main.py -i temp/100_input.txt -o temp/example.txt.hc -hc```
+
+decode
+
+```python3 main.py -i <input file path> -o <output_file_path> <algorithm -lz or -hz> -d```
+
+```python3 main.py -i temp/example.txt.hc -o temp/example.txt -hc -d```
+
+### Help
+```python3 main.py -h```
+
+```
+usage: Encode and decode files. Encodes by default [-h] [-i] [-o] [-lz] [-hc] [-d] [-p]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i, --input           Define input file path
+  -o, --output          Define output file path
+  -lz                   Use lempel-ziv algorithm
+  -hc                   Use Huffman coding algorithm
+  -d, --decode          Flag to decode specified file
+  -p, --performance_eval
+                        Runs performance evaluations from /samples folder. Will ignore all other parameters.
+```
--- src/main.py
+++ src/main.py
@@ -1,20 +1,47 @@
 import argparse
 import sys
+from huffman.huffman import HuffmanCoding
 from lz.lempel_ziv import LZ77
+from performance_eval import start_performance_eval
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', type=str, required=True)
-    parser.add_argument('-o', '--output', type=str, required=True)
-    parser.add_argument('-lz', action='store_true')
-    parser.add_argument('-hc', action='store_true')
-    parser.add_argument('-d', '--decode', action='store_true')
+    parser = argparse.ArgumentParser("Encode and decode files. Encodes by default")
+    parser.add_argument(
+        '-i', '--input', type=str,
+        help="Define input file path"
+    )
+    parser.add_argument(
+        '-o', '--output', type=str,
+        help="Define output file path"
+    )
+    parser.add_argument(
+        '-lz', action='store_true', help="Use lempel-ziv algorithm"
+    )
+    parser.add_argument(
+        '-hc', action='store_true', help="Use Huffman coding algorithm"
+    )
+    parser.add_argument(
+        '-d', '--decode', action='store_true',
+        help="Flag to decode specified file"
+    )
+    parser.add_argument(
+        '-p', '--performance_eval', action='store_true',
+        help="Runs performance evaluations from /samples folder. Will ignore all other parameters."
+    )
 
     args = parser.parse_args()
-
+    if args.performance_eval:
+        start_performance_eval()
+        print("Completer performance evaluations")
+        sys.exit(-1)
+        
     if not args.lz and not args.hc:
         print("No algorithm specified")
+        sys.exit(-1)
+
+    if not args.input or not args.output:
+        print("Input or output not specified")
         sys.exit(-1)
 
     if args.decode:
@@ -23,9 +50,19 @@ if __name__ == "__main__":
                 args.input,
                 args.output
             ).decode()
+        if args.hc:
+            HuffmanCoding(
+                args.input,
+                args.output
+            ).decode()
     else: #encode
         if args.lz:
             LZ77(
+                args.input,
+                args.output
+            ).encode()
+        if args.hc:
+            HuffmanCoding(
                 args.input,
                 args.output
             ).encode()
